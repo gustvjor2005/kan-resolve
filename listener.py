0bc5df3c8d4cdae11cc1082a82999e68466c2124
gchoque <gchoque@aepenergy.com>
2025-02-06 08:52:46 -0500

    add parameter path

--- listener.py
+++ listener.py
@@ -1,5 +1,17 @@
 import time
 from parse_trace import parse_log
+from argparse import ArgumentParser
+
+def main(arg):
+    path = arg.p
+    print(f"path is {path}")
+
+    try:
+        tail_log(path)
+    except KeyboardInterrupt:
+        print("Stopped monitoring logs.")
+
+    
 
 def tail_log(file_path):
     with open(file_path, 'r') as file:
@@ -9,14 +21,14 @@ def tail_log(file_path):
             if not line:
                 time.sleep(0.1)  # Wait for new data
                 continue
-            # print(line.strip())
             parse_log(line.strip())
 
 
 if __name__ == "__main__":
-    log_file = r"C:\Users\gustavo.choque\Documents\repo\ms-poc\auth-server\logs\application.json"
-    print(f"Watching {log_file} for new log entries...")
-    try:
-        tail_log(log_file)
-    except KeyboardInterrupt:
-        print("Stopped monitoring logs.")
+    parser = ArgumentParser()
+    parser.add_argument("-p", dest="p", type=str)
+    options = parser.parse_args()
+    print("OPTIONS", options)
+    main(options)
+    #log_file = r"C:\Users\s381201\Documents\repo\aepe-data-model-synchronizer\logs\application.json"    
+    #print(f"Watching {log_file} for new log entries...")
